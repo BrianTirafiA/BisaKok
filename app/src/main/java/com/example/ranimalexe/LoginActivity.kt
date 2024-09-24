6abf3eaef56117f4ebeb2168de0dd4f4e6d08486
lhanif <lhanif467@gmail.com>
2024-09-24 21:52:37 +0700

    Add Navigation

---
+++ app/src/main/java/com/example/ranimalexe/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.ranimalexe
+
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
+        getStartedButton.setOnClickListener {
+            val intent = Intent(this, ChooseAcc::class.java)
+            startActivity(intent)
+        }
+    }
+}
