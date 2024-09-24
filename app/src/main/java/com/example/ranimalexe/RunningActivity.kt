c5be74f58b9e0c78e15000ceb7e9a51b6af8bf0d
ekarahmadi <septianekarahmadi@365.ugm.ac.id>
2024-09-25 02:01:51 +0700

    add navigation bottom bar

--- app/src/main/java/com/example/ranimalexe/RunningActivity.kt
+++ app/src/main/java/com/example/ranimalexe/RunningActivity.kt
@@ -8,21 +8,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class RunningActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login)
+        setContentView(R.layout.bottom_navbar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-//        val getStartedButton = findViewById<Button>(R.id.loginButton)
-//        getStartedButton.setOnClickListener {
-//            val intent = Intent(this, ChooseAcc::class.java)
-//            startActivity(intent)
-//        }
     }
 }
