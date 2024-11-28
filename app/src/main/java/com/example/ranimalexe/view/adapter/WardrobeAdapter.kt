4edf0a7a70b2a9c80d79cad6e44e226be6bb5f32
BrianTirafiA <114814776+BrianTirafiA@users.noreply.github.com>
2024-11-28 20:21:02 +0700

    pet customizable

--- app/src/main/java/com/example/ranimalexe/view/adapter/WardrobeAdapter.kt
+++ app/src/main/java/com/example/ranimalexe/view/adapter/WardrobeAdapter.kt
@@ -7,30 +7,62 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ranimalexe.R
-import com.example.ranimalexe.model.WardrobeItem
+import com.example.ranimalexe.model.Hat
+import com.example.ranimalexe.model.Shell
 
-class WardrobeAdapter(private val wardrobeItemList: List<WardrobeItem>) : RecyclerView.Adapter<WardrobeAdapter.WardrobeItemViewHolder>() {
-    inner class WardrobeItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class HatAdapter(
+    private val hatItemList: List<Hat>,
+    private val onClick: (Int) -> Unit
+    ) : RecyclerView.Adapter<HatAdapter.HatItemViewHolder>() {
+    inner class HatItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val itemName: TextView = itemView.findViewById(R.id.itemName)
         private val itemImage: ImageView = itemView.findViewById(R.id.itemImage)
-        private val desc: TextView = itemView.findViewById(R.id.Desc)
 
-        fun bind(wardrobeItem: WardrobeItem) {
+        fun bind(wardrobeItem: Hat) {
             itemName.text = wardrobeItem.name
-            desc.text = wardrobeItem.desc
             itemImage.setImageResource(wardrobeItem.imageResId)
+            itemView.setOnClickListener { onClick(wardrobeItem.imageResId) }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WardrobeItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HatItemViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.wardrobe_item, parent, false)
-        return WardrobeItemViewHolder(itemView)
+        return HatItemViewHolder(itemView)
     }
 
-    override fun onBindViewHolder(holder: WardrobeItemViewHolder, position: Int) {
-        val wardrobeItem = wardrobeItemList[position]
+    override fun onBindViewHolder(holder: HatItemViewHolder, position: Int) {
+        val wardrobeItem = hatItemList[position]
         holder.bind(wardrobeItem)
     }
 
-    override fun getItemCount(): Int = wardrobeItemList.size
+    override fun getItemCount(): Int = hatItemList.size
 }
+
+class ShellAdapter(
+    private val shellItemList: List<Shell>,
+    private val onClick: (Int) -> Unit
+) : RecyclerView.Adapter<ShellAdapter.ShellItemViewHolder>() {
+    inner class ShellItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val itemName: TextView = itemView.findViewById(R.id.itemName)
+        private val itemImage: ImageView = itemView.findViewById(R.id.itemImage)
+
+        fun bind(wardrobeItem: Shell) {
+            itemName.text = wardrobeItem.name
+            itemImage.setImageResource(wardrobeItem.imageResId)
+            itemView.setOnClickListener { onClick(wardrobeItem.imageResId) }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShellItemViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.wardrobe_item, parent, false)
+        return ShellItemViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ShellItemViewHolder, position: Int) {
+        val shellItem = shellItemList[position]
+        holder.bind(shellItem)
+    }
+
+    override fun getItemCount(): Int = shellItemList.size
+}
+
--- app/src/main/java/com/example/ranimalexe/viewmodel/WardrobeViewModel.kt
+++ app/src/main/java/com/example/ranimalexe/viewmodel/WardrobeViewModel.kt
@@ -4,22 +4,74 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.ranimalexe.R
-import com.example.ranimalexe.model.WardrobeItem
-
-class WardrobeViewModel : ViewModel() {
-    private val _cosmetic = MutableLiveData<List<WardrobeItem>>()
-    val cosmeticList: LiveData<List<WardrobeItem>> get() = _cosmetic
+import com.example.ranimalexe.model.Hat
+import com.example.ranimalexe.model.PetCustomization
+import com.example.ranimalexe.model.Shell
+class HatViewModel : ViewModel() {
+    private val _hat = MutableLiveData<List<Hat>>()
+    val hatList: LiveData<List<Hat>> get() = _hat
 
     init {
         loadCosmetic()
     }
 
     private fun loadCosmetic() {
-        _cosmetic.value = listOf(
-            WardrobeItem(1, "MeteorHead", R.drawable.meteorhead, "Coming for extinction", false),
-            WardrobeItem(2, "Beret", R.drawable.beret, "Honourable soldier", true),
-            WardrobeItem(3, "Cosmic Shell", R.drawable.cometshell, "it radiates cosmic power", false),
-            WardrobeItem(4, "Veteran Shell", R.drawable.milishell, "Ghost of battlefield", true),
+        _hat.value = listOf(
+            Hat(0, "Basic Red", R.drawable.default_hat1, "proof of your birth", false),
+            Hat(1, "Meteor", R.drawable.meteorhead, "Coming for extinction", false),
+            Hat(2, "Beret", R.drawable.beret, "Honourable soldier", true),
+            Hat(3, "Meteor", R.drawable.meteorhead, "Coming for extinction", false),
+            Hat(4, "Beret", R.drawable.beret, "Honourable soldier", true),
+            Hat(5, "Meteor", R.drawable.meteorhead, "Coming for extinction", false),
+            Hat(6, "Beret", R.drawable.beret, "Honourable soldier", true),
+            Hat(7, "Meteor", R.drawable.meteorhead, "Coming for extinction", false),
+            Hat(8, "Beret", R.drawable.beret, "Honourable soldier", true),
+            Hat(9, "Meteor", R.drawable.meteorhead, "Coming for extinction", false),
+            Hat(10, "Beret", R.drawable.beret, "Honourable soldier", true),
         )
+    }
+}
+
+class ShellViewModel : ViewModel() {
+    private val _shell = MutableLiveData<List<Shell>>()
+    val shellList: LiveData<List<Shell>> get() = _shell
+
+    init {
+        loadCosmetic()
+    }
+
+    private fun loadCosmetic() {
+        _shell.value = listOf(
+            Shell(0, "Basic Red", R.drawable.default_shell1, "proof of your birth", false),
+            Shell(1, "Cosmic", R.drawable.cometshell, "it radiates cosmic power", false),
+            Shell(2, "Veteran", R.drawable.milishell, "Ghost of battlefield", true),
+            Shell(3, "Cosmic", R.drawable.cometshell, "it radiates cosmic power", false),
+            Shell(4, "Veteran", R.drawable.milishell, "Ghost of battlefield", true),
+            Shell(5, "Cosmic", R.drawable.cometshell, "it radiates cosmic power", false),
+            Shell(6, "Veteran", R.drawable.milishell, "Ghost of battlefield", true),
+            Shell(7, "Cosmic", R.drawable.cometshell, "it radiates cosmic power", false),
+            Shell(8, "Veteran", R.drawable.milishell, "Ghost of battlefield", true),
+            Shell(9, "Cosmic", R.drawable.cometshell, "it radiates cosmic power", false),
+            Shell(10, "Veteran", R.drawable.milishell, "Ghost of battlefield", true),
+        )
+    }
+}
+
+
+class PetViewModel : ViewModel() {
+
+    private val _customization = MutableLiveData(PetCustomization())
+    val customization: LiveData<PetCustomization> get() = _customization
+
+    fun updatePet(pet: Int) {
+        _customization.value = _customization.value?.copy(pet = pet)
+    }
+
+    fun updateClothes(clothes: Int) {
+        _customization.value = _customization.value?.copy(clothes = clothes)
+    }
+
+    fun updateHat(hat: Int) {
+        _customization.value = _customization.value?.copy(hat = hat)
     }
 }
